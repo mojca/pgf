ad70b36e6c1000d2d453c3d7ee09339c8d426cf3
jannis-pohlmann <jannis-pohlmann>
2011-05-02 02:25:22 +0000

    - Strip redundant semi-colon.

--- generic/pgf/libraries/graphdrawing/lua/pgflibrarygraphdrawing-node.lua
+++ generic/pgf/libraries/graphdrawing/lua/pgflibrarygraphdrawing-node.lua
@@ -8,7 +8,7 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more information
 
--- @release $Header: /home/mojca/cron/mojca/github/cvs/pgf/pgf/generic/pgf/libraries/graphdrawing/lua/Attic/pgflibrarygraphdrawing-node.lua,v 1.7 2011/05/02 02:20:36 jannis-pohlmann Exp $
+-- @release $Header: /home/mojca/cron/mojca/github/cvs/pgf/pgf/generic/pgf/libraries/graphdrawing/lua/Attic/pgflibrarygraphdrawing-node.lua,v 1.8 2011/05/02 02:25:23 jannis-pohlmann Exp $
 
 -- This file defines a node class, used in the graph representation.
 
@@ -161,7 +161,7 @@ end
 -- @return True if self is equal to object.
 function Node:__eq(object)
    Sys:logMessage('LAY: node eq function called')
-   return self.name == object.name;
+   return self.name == object.name
 end
 
 --- Returns a formated string representation of the node.
