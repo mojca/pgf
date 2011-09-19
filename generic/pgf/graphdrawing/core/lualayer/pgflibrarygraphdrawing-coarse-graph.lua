7f5545f59574940b89438b89da85012c98b2575c
jannis-pohlmann <jannis-pohlmann>
2011-09-19 22:48:12 +0000

    graph drawing: Make several changes in Hu2006 spring.
    Most notably, the adaptive cooling scheme is now used in the
    single-level version of the algorithm and the force proposed by Hu is
    used rather than my own very artificial force function.

--- generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-coarse-graph.lua
+++ generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-coarse-graph.lua
@@ -7,7 +7,7 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more information
 
--- @release $Header: /home/mojca/cron/mojca/github/cvs/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-coarse-graph.lua,v 1.4 2011/07/20 21:01:27 jannis-pohlmann Exp $
+-- @release $Header: /home/mojca/cron/mojca/github/cvs/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-coarse-graph.lua,v 1.5 2011/09/19 22:48:12 jannis-pohlmann Exp $
 
 pgf.module("pgf.graphdrawing")
 
@@ -235,7 +235,7 @@ function CoarseGraph:coarsen()
         -- replace the old edges
         self.graph:addEdge(superedge)
         for edge in table.value_iter(edges) do
-          Sys:log('    delete edge ' .. tostring(edge))
+          --Sys:log('    delete edge ' .. tostring(edge))
           self.graph:deleteEdge(edge)
         end
       end
@@ -422,8 +422,11 @@ function CoarseGraph:findMaximalMatching()
         return not matched_nodes[edge:getNeighbour(node)]
       end)
 
+      -- FIXME TODO We use a light-vertex matching here. This is
+      -- different from the algorithm proposed by Hu which collapses
+      -- edges based on a heavy-edge matching...
       if #edges > 0 then
-        -- sort edges bby the weights of the node's neighbours
+        -- sort edges by the weights of the node's neighbours
         table.sort(edges, function (a, b)
           return a:getNeighbour(node).weight < b:getNeighbour(node).weight
         end)
