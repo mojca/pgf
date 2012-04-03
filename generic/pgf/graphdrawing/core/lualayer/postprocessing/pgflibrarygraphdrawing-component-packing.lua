b9a447d85c628a61f0aba9897cafaa8a906a80d8
tantau <tantau>
2012-04-03 21:41:43 +0000

    moved files into subdirectories

---
+++ generic/pgf/graphdrawing/core/lualayer/postprocessing/pgflibrarygraphdrawing-component-packing.lua
@@ -0,0 +1,19 @@
+-- Copyright 2011 by Jannis Pohlmann
+--
+-- This file may be distributed and/or modified
+--
+-- 1. under the LaTeX Project Public License and/or
+-- 2. under the GNU Public License
+--
+-- See the file doc/generic/pgf/licenses/LICENSE for more information
+
+-- @release $Header: /home/mojca/cron/mojca/github/cvs/pgf/pgf/generic/pgf/graphdrawing/core/lualayer/postprocessing/pgflibrarygraphdrawing-component-packing.lua,v 1.1 2012/04/03 21:41:45 tantau Exp $
+
+pgf.module("pgf.graphdrawing")
+
+componentpacking = {}
+
+
+
+function componentpacking.pack(graph)
+end
